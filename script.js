cf3f2743eae14a31e3dc0c9a5e954861bf5d9834
Irfan Ali <ia.ali053263@gmail.com>
2023-06-15 13:56:08 +0530

    categories product by filters

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function filterProductByCategory(category) {
     console.log(category);
 }
+
+var divs = ["All-item", "women", "all-men-list", "all-kid-items","all-accessories","all-cosmetics"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if(visibleDivId === divId) {
+    //visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for(i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if(visibleDivId === divId) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
