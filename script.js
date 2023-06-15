416e45da7eeca1037e899a25b26e8aa71abea408
Irfan Ali <ia.ali053263@gmail.com>
2023-06-15 15:56:24 +0530

    filter product by category

--- script.js
+++ script.js
@@ -2,25 +2,15 @@ function filterProductByCategory(category) {
     console.log(category);
 }
 
-var divs = ["All-item", "women", "all-men-list", "all-kid-items","all-accessories","all-cosmetics"];
-var visibleDivId = null;
+var divs = ["all-item", "women", "all-men-list", "all-kid-items","all-accessories","all-cosmetics"];
+
 function filterProductByCategory(divId) {
-  if(visibleDivId === divId) {
-    //visibleDivId = null;
-  } else {
-    visibleDivId = divId;
-  }
-  hideNonVisibleDivs();
-}
-function hideNonVisibleDivs() {
-  var i, divId, div;
   for(i = 0; i < divs.length; i++) {
-    divId = divs[i];
-    div = document.getElementById(divId);
-    if(visibleDivId === divId) {
-      div.style.display = "block";
+    element = document.getElementById(divs[i]);
+    if (divs[i] === divId) {
+      element.style.display = "block";
     } else {
-      div.style.display = "none";
+      element.style.display = "none";
     }
   }
 }
