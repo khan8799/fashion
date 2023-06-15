cd498cdce08d963308fd1bd5ca5ec69b3510a096
Sania Javed <sania.javed.789@gmail.com>
2023-06-15 22:23:20 +0530

    Created Navbar Links Using JS

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 var linkIds = [
   {
     id: "all-item",
-    text: 'All'
+    text: "All",
   },
   {
     id: "women",
-    text: 'Womens'
+    text: "Womens",
   },
   {
     id: "all-men-list",
-    text: 'Mens'
+    text: "Mens",
   },
   {
     id: "all-kid-items",
-    text: 'Kids'
+    text: "Kids",
   },
   {
     id: "all-accessories",
-    text: 'Accessories'
+    text: "Accessories",
   },
   {
     id: "all-cosmetics",
-    text: 'Cosmetics',
-  }
+    text: "Cosmetics",
+  },
 ];
 
 function filterProductByCategory(divId) {
-  for(i = 0; i < linkIds.length; i++) {
+  for (i = 0; i < linkIds.length; i++) {
     element = document.getElementById(linkIds[i].id);
     if (linkIds[i].id === divId) {
       element.style.display = "block";
@@ -36,20 +36,67 @@ function filterProductByCategory(divId) {
   }
 }
 
-const categoryElement = document.getElementById('categories');
+const categoryElement = document.getElementById("categories");
 
-linkIds.forEach(link => {
+linkIds.forEach((link) => {
   categoryElement.appendChild(createCategoriesLink(link));
 });
 
 function createCategoriesLink(link) {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
 
-  li.classList.add('nav-item');
+  li.classList.add("nav-item");
 
-  const anchor = document.createElement('a');
+  const anchor = document.createElement("a");
   anchor.innerText = link.text;
-  anchor.addEventListener('click', () => filterProductByCategory(link.id));
+  anchor.addEventListener("click", () => filterProductByCategory(link.id));
   li.appendChild(anchor);
   return li;
 }
+
+var navbarLinkTabs = [
+  {
+    text: "HOME",
+    link: "./index.html",
+  },
+  {
+    text: "WOMEN'S",
+    link: "javascript:void()",
+  },
+  {
+    text: "MEN'S",
+    link: "javascript:void()",
+  },
+  {
+    text: "BLOG",
+    link: "javascript:void()",
+  },
+  {
+    text: "SHOP",
+    link: "javascript:void()",
+  },
+  {
+    text: "CONTACT",
+    link: "./contactUs.html",
+  },
+];
+
+var navElement = document.getElementById("navbarLinks");
+
+navbarLinkTabs.forEach(function (url) {
+  navElement.appendChild(createNavbarLink(url));
+});
+
+function createNavbarLink(url) {
+  const li = document.createElement('li');
+  li.classList.add('nav-item');
+
+  const anchor = document.createElement('a');
+  anchor.innerText = url.text;
+  anchor.classList.add('nav-link');
+  anchor.href = url.link;
+  li.appendChild(anchor);
+  
+  return li;
+}
+
