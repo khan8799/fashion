a0c7aa4e5d75c41cd76cac7ec7ab1d6a4e8bdb4b
aliakbar <ali.rocking277@gmail.com>
2023-06-15 15:00:15 +0530

    hiding features is added

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function filterProductByCategory(category) {
-    console.log(category);
+   
+   var arr=['all','women','men','kid','accessories','cosmetic'];
+   var arrr=['data1','data2','data3','data4','data5','data6'];
+  for(var i=0;i<arr.length;i++)
+  {
+    if(category==arr[0])
+    {
+        document.getElementById(arrr[i]).style.display = "block";
+    }
+ else  if(category==arr[i])
+    {
+        document.getElementById(arrr[i]).style.display = "block";
+    }
+    else
+    {
+        document.getElementById(arrr[i]).style.display = "none";
+    
+   }
+}
 }
